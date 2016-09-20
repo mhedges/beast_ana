f44c83446afd3bf80974ea3dffadf60c3d61fe91
Michael Hedges <mhedges@hawaii.edu>
2016-09-20 11:53:35 -1000

    Added first template of python analysis script

---
+++ ana.py
@@ -0,0 +1,14 @@
+import os
+import seaborn
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from root_numpy import root2rec
+
+def main():
+    
+    datapath = ''
+
+if __name__ == "__main__":
+    main()
